c123a15e66c233cddb7799ec297f9213f9ff570a
Waipang Fong <waipang.fong@itcadre.com>
2015-11-19 14:51:14 -0500

    move the url to index

--- spike/urls.py
+++ spike/urls.py
@@ -2,9 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.index, name='index'),
 	url(r'^fda$', views.fda, name='fda'),
-	url(r'^search-form/$', views.search_form, name='search_form'),
+	url(r'^$', views.search_form, name='search_form'),
 	url(r'^search/$', views.search, name='search'),
 	url(r'^get-drug-names/$', views.get_drug_names, name='get_drug_names'),
 ]
