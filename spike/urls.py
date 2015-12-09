872e3e3b5e4bfdc113a5a5b027ff83045168fea7
Waipang Fong <waipang.fong@itcadre.com>
2015-12-09 16:20:52 -0500

    forgot to update the url

--- spike/urls.py
+++ spike/urls.py
@@ -6,7 +6,9 @@ urlpatterns = [
     url(r'^fda$', views.fda, name='fda'),
     url(r'^$', views.search_form, name='search_form'),
     url(r'^search/$', views.search, name='search'),
-    url(r'^get-drug-names/$', views.get_drug_names, name='get_drug_names')
+    url(r'^get-drug-names/$', views.get_drug_names, name='get_drug_names'),
+    url(r'^search-mentioned-events-twitter/$', views.search_mentioned_events_twitter,
+        name='search_mentioned_events_twitter')
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
